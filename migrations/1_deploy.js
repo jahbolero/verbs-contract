b99aa8061a92683790d0787632dfd1a777d88bb0
jahbolero <uchiha4322@hotmail.com>
2022-05-15 17:37:59 +0800

    Succes deploy

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,5 +1,5 @@
 const Verb = artifacts.require("Verb");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(Verb, 1652198400, 1652209200, 1652295600, 1652295600);
+  await deployer.deploy(Verb, 1652630400, 1652644801, 1652731200, 1652731200);
 };
